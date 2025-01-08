878c1382e1652aaa5ffcb71c5ac069f84acfe3c7
AndersKabel <kabel@mvbmail.dk>
2025-01-08 21:34:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,10 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 map.on('click', function(e) {
     var lat = e.latlng.lat;
     var lon = e.latlng.lng;
-
-    // Tilføj parameteren "struktur=flad" for et simplere JSON-svar
-    fetch(`https://api.dataforsyningen.dk/adgangsadresser/reverse?x=${lon}&y=${lat}&struktur=flad`)
+    fetch(`https://api.dataforsyningen.dk/adgangsadresser/reverse?x=${lon}&y=${lat}`)
         .then(response => response.json())
         .then(data => {
-            // Tjek om data indeholder de forventede felter
-            if (data.vejnavn && data.husnr && data.postnr && data.postnrnavn) {
-                document.getElementById('address').innerText = `Adresse: ${data.vejnavn} ${data.husnr}, ${data.postnr} ${data.postnrnavn}`;
-            } else {
-                document.getElementById('address').innerText = "Adresse kunne ikke findes.";
-            }
+            document.getElementById('address').innerText = `Adresse: ${data.vejnavn} ${data.husnr}, ${data.postnr} ${data.postnrnavn}`;
         })
-        .catch(err => {
-            console.error(err);
-            document.getElementById('address').innerText = "Der opstod en fejl med reverse geocoding.";
-        });
+        .catch(err => console.error(err));
 });
