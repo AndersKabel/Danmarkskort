4600b7bed22fadfd5c96827b20ee891dc9fa4c4c
AndersKabel <kabel@mvbmail.dk>
2025-01-08 22:14:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+// Initialiser kortet
 var map = L.map('map').setView([56, 10], 7);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
+// Variabel til at gemme den aktuelle markør
+var currentMarker;
+
 map.on('click', function(e) {
     var lat = e.latlng.lat;
     var lon = e.latlng.lng;
 
-    // Fjern tidligere markører (valgfrit, hvis du vil undgå flere markører)
-    if (typeof marker !== 'undefined') {
-        map.removeLayer(marker);
+    // Fjern den tidligere markør, hvis den findes
+    if (currentMarker) {
+        map.removeLayer(currentMarker);
     }
 
-    // Tilføj en ny markør på kortet
-    var marker = L.marker([lat, lon]).addTo(map);
+    // Tilføj en ny markør og gem den i variablen
+    currentMarker = L.marker([lat, lon]).addTo(map);
 
+    // Hent adresse via reverse geocoding
     fetch(`https://api.dataforsyningen.dk/adgangsadresser/reverse?x=${lon}&y=${lat}&struktur=flad`)
         .then(response => response.json())
         .then(data => {
