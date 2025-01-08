f9d5f386d7f87e47d8584b9f8aaff57f9460f646
AndersKabel <kabel@mvbmail.dk>
2025-01-08 21:41:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-// Opret et kort og centrer det over Danmark
-var map = L.map('map').setView([56, 10], 7); // Koordinater for Danmark
-
-// Tilføj OpenStreetMap-fliser
+var map = L.map('map').setView([56, 10], 7);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; OpenStreetMap contributors'
+    maxZoom: 19
 }).addTo(map);
+
+map.on('click', function(e) {
+    var lat = e.latlng.lat;
+    var lon = e.latlng.lng;
+    fetch(`https://api.dataforsyningen.dk/adgangsadresser/reverse?x=${lon}&y=${lat}`)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('address').innerText = `Adresse: ${data.vejnavn} ${data.husnr}, ${data.postnr} ${data.postnrnavn}`;
+        })
+        .catch(err => console.error(err));
+});
