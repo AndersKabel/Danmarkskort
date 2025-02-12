c0aa582e55e0491988c6effdd5b180eaaf1d724c
AndersKabel <kabel@mvbmail.dk>
2025-02-12 22:51:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -44,8 +44,8 @@ document.getElementById('search').addEventListener('input', function () {
     }
     if (query.length < 2) return;
 
-    const username = NUKALQTAFO;  // Erstat med dit brugernavn
-const password = Fw62huch!; // Erstat med din adgangskode
+    const username = "NUKALQTAFO";  // Erstat med dit brugernavn
+const password = "Fw62huch!"; // Erstat med din adgangskode
 const credentials = btoa(`${username}:${password}`); // Base64-kodning
 
 Promise.all([
