3eb72435c094b5df1af8dbb3676c8c0482aa47c8
AndersKabel <kabel@mvbmail.dk>
2025-03-03 11:57:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -218,8 +218,8 @@ function doSearch(query, listElement) {
                             console.log("Koordinater fra addressData:", x, y, "=> efter konvertering:", coords);
                             console.log("Koordinater før og efter konvertering:", x, y, coords);
                             // lat = coords[1], lon = coords[0]
-                            let lat = coords[0];
-                            let lon = coords[1];
+                            let lat = coords[1];
+                            let lon = coords[0];
                             console.log("Endelige koordinater til placering:", lat, lon);
 
                             // => Kald placeMarkerAndZoom med [lat, lon] (y først, x sidst)
