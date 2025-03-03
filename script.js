4668319b81b73ddea449deb821a70c4c267c7714
AndersKabel <kabel@mvbmail.dk>
2025-03-03 11:04:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
  ***************************************************/
 proj4.defs("EPSG:25832", "+proj=utm +zone=32 +ellps=GRS80 +datum=WGS84 +units=m +no_defs");
 function convertToWGS84(x, y) {
-    let result = proj4("EPSG:25832", "EPSG:4326", [y, x]); // Bytter x og y
-    console.log("convertToWGS84 rettet input:", x, y, "=> output:", result);
+    let result = proj4("EPSG:25832", "EPSG:4326", [x, y]); // Korrekt rækkefølge
+    console.log("convertToWGS84 input:", x, y, "=> output:", result);
     return result;
 }
 
