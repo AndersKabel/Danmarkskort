7c68bfca976060c63e9a54b168a3a787ec9c0b71
AndersKabel <kabel@mvbmail.dk>
2025-03-14 08:26:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ async function updateInfoBox(data, lat, lon) {
     const vej2List = document.getElementById("results-vej2");
 
     const adresseStr = `${data.vejnavn || "?"} ${data.husnr || ""}, ${data.postnr || "?"} ${data.postnrnavn || ""}`;
-    const ekstraInfoStr = `Kommunekode: ${data.kommunekode || "?"} | Vejkode: ${data.vejkode || "?"}`;
+const ekstraInfoStr = `Kommunekode: ${data.kommunekode || "?"} | Vejkode: ${data.vejkode || "?"}`;
 
     streetviewLink.href = `https://www.google.com/maps?q=&layer=c&cbll=${lat},${lon}`;
     addressEl.textContent = adresseStr;
