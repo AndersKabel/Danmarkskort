0b263b7b16d982320ef513eec61615d3d0e1c2c9
AndersKabel <kabel@mvbmail.dk>
2025-04-23 08:48:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -192,6 +192,14 @@ const overlayMaps = {
   "Strandposter": redningsnrLayer,
   "Falck Ass": falckAssLayer,
   "Kommunegrænser": kommunegrænserLayer
+    "Dyrenes Beskyttelse": {
+    onAdd: function () {
+      window.open("https://kort.dyrenesbeskyttelse.dk/db/dvc.nsf/kort", "_blank");
+      // Returner en tom Leaflet layer group så Leaflet ikke fejler
+      return L.layerGroup();
+    },
+    onRemove: function () {}
+  },
 };
 
 L.control.layers(baseMaps, overlayMaps, { position: 'topright' }).addTo(map);
